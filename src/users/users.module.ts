23a2af288dc350667f208cfa4954070d9dcfdf4f
Ozan <ozancck@icloud.com>
2022-07-29 22:25:57 +0300

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service';
 import { User } from './user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService]
 })
