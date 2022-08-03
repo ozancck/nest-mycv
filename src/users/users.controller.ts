22bb0f88f5767ba8c7cceff0f06772408ec9d13b
Ozan <ozancck@icloud.com>
2022-08-03 23:24:23 +0300

    class validator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body,Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
 
     @Post('/signup')
-    createUser(){
+    createUser(@Body() body:CreateUserDto){
+        console.log(body);
         
     }
 }
